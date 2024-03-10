39cc2162d5c8ce656c1f6b5475ad3acd01744154
obede-cossa <obedecossa@mozconnections.co.mz>
2024-03-10 13:56:28 +0200

    feat: typing allowrd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 
 wppconnect.create({
-  session: 'sessionName', //Pass the name of the client you want to start the bot
+  session: 'sessionName',
   catchQR: (base64Qrimg, asciiQR, attempts, urlCode) => {
     console.log('Number of attempts to read the qrcode: ', attempts);
     console.log('Terminal qrcode: ', asciiQR);
@@ -9,22 +9,21 @@ wppconnect.create({
     console.log('urlCode (data-ref): ', urlCode);
   },
   statusFind: (statusSession, session) => {
-    console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
-    //Create session wss return "serverClose" case server for close
+    console.log('Status Session: ', statusSession); 
     console.log('Session name: ', session);
   },
-  headless: true, // Headless chrome
-  devtools: false, // Open devtools by default
-  useChrome: true, // If false will use Chromium instance
-  debug: false, // Opens a debug session
-  logQR: true, // Logs QR automatically in terminal
-  browserWS: '', // If u want to use browserWSEndpoint
-  browserArgs: [''], // Parameters to be added into the chrome browser instance
-  puppeteerOptions: {}, // Will be passed to puppeteer.launch
-  disableWelcome: false, // Option to disable the welcoming message which appears in the beginning
-  updatesLog: true, // Logs info updates automatically in terminal
-  autoClose: 60000, // Automatically closes the wppconnect only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
-  tokenStore: 'file', // Define how work with tokens, that can be a custom interface
+  headless: true,
+  devtools: false,
+  useChrome: true,
+  debug: false, 
+  logQR: true, 
+  browserWS: '', 
+  browserArgs: [''],
+  puppeteerOptions: {}, 
+  disableWelcome: false, 
+  updatesLog: true, 
+  autoClose: 60000, 
+  tokenStore: 'file', 
   folderNameToken: './tokens', 
   sessionToken: {
     WABrowserId: '"UnXjH....."',
@@ -48,19 +47,17 @@ function start(client) {
     phone: usernumber,
     location: "Zimpeto",
    }
-   console.log(user)
     client
+      .startTyping(message.from)
       .sendText(message.from, await askBot(message.body, user))
-
+      .stopTyping(message.from)
+      
   });
 }
 
 
 async function askBot(prompt, user){
   try{
-  
-    
-
       const response = await fetch('https://edmbotapi.onrender.com/ai/edmbot-test/stream/ask', {
       method: "post",
       credentials: 'include',
