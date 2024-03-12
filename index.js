c95bb5aeb51e8037adfdddc1e25208a6f9adb8df
obede-cossa <obedecossa@mozconnections.co.mz>
2024-03-12 16:12:33 +0200

    Feat and Fix
    feat: typing support; fix: responding status

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ wppconnect.create({
     console.log('urlCode (data-ref): ', urlCode);
   },
   statusFind: (statusSession, session) => {
-    console.log('Status Session: ', statusSession); 
+    console.log('Status Session: ', statusSession);
     console.log('Session name: ', session);
   },
   headless: true,
   devtools: false,
   useChrome: true,
-  debug: false, 
-  logQR: true, 
-  browserWS: '', 
+  debug: false,
+  logQR: true,
+  browserWS: '',
   browserArgs: [''],
-  puppeteerOptions: {}, 
-  disableWelcome: false, 
-  updatesLog: true, 
-  autoClose: 60000, 
-  tokenStore: 'file', 
-  folderNameToken: './tokens', 
+  puppeteerOptions: {},
+  disableWelcome: false,
+  updatesLog: true,
+  autoClose: 60000,
+  tokenStore: 'file',
+  folderNameToken: './tokens',
   sessionToken: {
     WABrowserId: '"UnXjH....."',
     WASecretBundle: '{"key":"+i/nRgWJ....","encKey":"kGdMR5t....","macKey":"+i/nRgW...."}',
@@ -32,40 +32,45 @@ wppconnect.create({
     WAToken2: '"1@lPpzwC...."',
   }
 })
-.then((client) => start(client))
-.catch((error) => console.log(error));
+  .then((client) => start(client))
+  .catch((error) => console.log(error));
 
 
 function start(client) {
-  client.onMessage(async message=> {
-   console.log(message)
-   const userId = message.sender.id;
-   const usernumber = userId.split("@")[0]
-   const user = {
-    id: message.from,
-    name: message.sender.pushname,
-    phone: usernumber,
-    location: "Zimpeto",
-   }
-    client
-      .startTyping(message.from)
-      .sendText(message.from, await askBot(message.body, user))
-      .stopTyping(message.from)
-      
+  client.onMessage(async message => {
+    // console.log(message)
+    const userId = message.sender.id;
+    const usernumber = userId.split("@")[0]
+    const user = {
+      id: message.from,
+      name: message.sender.pushname,
+      phone: usernumber,
+      location: "Zimpeto",
+    }
+
+    if (message.from != 'status@broadcast') {
+      client.startTyping(message.from)
+      message.type == 'sticker' ?
+        client.sendText(message.from, "Desculpe, eu não tenho suporte para stickers")
+        :
+        client.sendText(message.from, await askBot(message.body, user))
+
+      client.stopTyping(message.from)
+    }
   });
 }
 
 
-async function askBot(prompt, user){
-  try{
-      const response = await fetch('https://edmbotapi.onrender.com/ai/edmbot-test/stream/ask', {
+async function askBot(prompt, user) {
+  try {
+    const response = await fetch('https://edmbotapi.onrender.com/ai/edmbot-test/stream/ask', {
       method: "post",
       credentials: 'include',
       headers: {
         'Authorization': 'dev-aidbuikacalwgdconnamwqirycnvacpaevsbmsoaadaczawacawbbacf-team',
         'Content-type': 'application/json'
       },
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         prompt,
         user,
         stream: false
@@ -73,13 +78,13 @@ async function askBot(prompt, user){
     });
 
     const text = await response.text()
-   console.log(text)
+    console.log(text)
 
-   return text
-    
+    return text
+
   } catch (error) {
     console.error(error);
-        bot.sendMessage(msg.from.id, `Invalid url, Try sending only the url you pretend to short`)
+    bot.sendMessage(msg.from.id, `Invalid url, Try sending only the url you pretend to short`)
   }
 }
 
@@ -102,9 +107,11 @@ async function askBot(prompt, user){
 //   client
 //     .sendText('258821001421@c.us', `The client ${message.sender.verifiedName}, needs help. wa.me/+${usernumber}`)
 //     .then((result) => {
-//       console.log('Result: ', result); 
+//       console.log('Result: ', result);
 //     })
 //     .catch((erro) => {
-//       console.error('Error when sending: ', erro); 
+//       console.error('Error when sending: ', erro);
 //     });
 // }
+
+
---
+++ teste.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+const num2 = 20;
+
+console.log(num1)
+
+num1 = false;
+
+console.log(num1)
+
+num1 = "liedson"
+
+console.log(num1)
